944732e009ab4c8fd696888db9f68464f23e7b0c
socivDev <clementlaw0808@gmai.com>
2025-04-29 05:52:58 +0800

    Base

---
+++ app/components/about.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return(
+        <div>
+            <section id='hero'>
+                <div>Hi</div>
+            </section>
+        </div>
+    )
+}
+
---
+++ app/components/hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+    return(
+        <div>
+            <section id='hero'>
+                <div>Hi</div>
+            </section>
+        </div>
+    )
+}
+
